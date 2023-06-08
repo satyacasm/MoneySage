ce1db10bed453492e9a07ae73436fd522ed17a67
AniModi <98632268+AniModi@users.noreply.github.com>
2023-06-08 23:20:51 +0530

    added login and signup page

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container');
+
+function onSignUp() {
+  container.classList.add('right-panel-active');
+}
+
+function onSignIn() {
+  container.classList.remove('right-panel-active');
+}
