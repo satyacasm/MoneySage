3cfc3f1c691033cbb1bdd57b37ccef4ac732e887
satyacasm <202151141@iiitvadodara.ac.in>
2023-06-18 16:28:56 +0530

    Re initialising repository

--- frontend/app.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('container');
-
-function onSignUp() {
-  container.classList.add('right-panel-active');
-}
-
-function onSignIn() {
-  container.classList.remove('right-panel-active');
-}
